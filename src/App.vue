ba66799138322eb188da0228fabf5c8a007e2402
Sam <samuv.eth@gmail.com>
2023-08-10 20:17:35 +0700

    Add mrp

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useInfiniteScroll } from '@vueuse/core'
+
+const data = ref([1])
+
+useInfiniteScroll(
+  document,
+  () => {
+    console.log('🚀 ~ file: App.vue:10 ~ document:', document)
+    const length = data.value.length + 1
+    data.value.push(...Array.from({ length: 5 }, (_, i) => length + i))
+  },
+  { distance: 100 }
+)
+</script>
 
 <template>
-  <div class="h-screen text-red-500">test</div>
+  <div class="flex flex-col gap-2 p-4">
+    <div v-for="item in data" :key="item" class="h-32 bg-gray-500/5 rounded p-3">
+      {{ item }}
+    </div>
+  </div>
 </template>
